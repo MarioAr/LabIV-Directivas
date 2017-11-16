3e2c764d177ff29d1ed96f720a38af1ba5d54cf2
Mario Rampi <MarioAr@users.noreply.github.com>
2017-11-16 10:19:18 -0300

    Update color.directive.ts

--- src/app/directivas/color.directive.ts
+++ src/app/directivas/color.directive.ts
@@ -6,15 +6,17 @@ import { Directive, ElementRef, OnInit, Renderer2, Input } from '@angular/core';
 export class ColorDirective implements OnInit {
 
   @Input() appColor: string;
-
+  mensaje = 'Ingreso el color: ';
+  
   ngOnInit(): void {
     console.log(this.el);
     if ( !this.appColor ) {
       this.appColor = 'pink';
+      this.mensaje = 'Color por default: ';
     }
     this.el.nativeElement.innerText = '';
     // this.renderer.setProperty(this.el.nativeElement, 'textContent', '');
-    const text = this.renderer.createText('Ingreso el color: ' + this.appColor);
+    const text = this.renderer.createText(this.mensaje + this.appColor);
     this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', this.appColor);
     this.renderer.appendChild(this.el.nativeElement, text);
 
